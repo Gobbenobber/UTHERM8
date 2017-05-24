39a80ab43643bcaf92d36c733291b5336e11c93a
Gobbenobber <gobbenobber@gmail.com>
2017-05-25 00:08:40 +0200

    It builds and downloads.
    Yay, I figured out how to serial program my arduino with the project.
    I'll retire for today...

--- LEKTOR/LEKTOR_MIKROCONTROLLER/LEKTOR_MIKROCONTROLLER/Debug/makedep.mk
+++ LEKTOR/LEKTOR_MIKROCONTROLLER/LEKTOR_MIKROCONTROLLER/Debug/makedep.mk
@@ -2,7 +2,7 @@
 # Automatically-generated file. Do not edit or delete the file
 ################################################################################
 
-main.c
+LEKTOR_MIKROCONTROLLER.c
 
 Manchester.c
 
