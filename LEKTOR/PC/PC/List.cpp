7695fabd67616ed13ca2fdde3ee2d8a48b6167f6
reklezz <drelixgaming@gmail.com>
2017-05-24 09:13:06 +0200

    updated List

--- LEKTOR/PC/PC/List.cpp
+++ LEKTOR/PC/PC/List.cpp
@@ -10,3 +10,53 @@ List::List()
 List::~List()
 {
 }
+
+void List::setSize(int size)
+{
+}
+
+int List::getSize() const
+{
+	return size_;
+}
+
+void List::setLine(std::string line)
+{
+}
+
+std::string List::getLine() const
+{
+	return line_;
+}
+
+void List::initializeSpaces(int size)
+{
+}
+
+void List::printAll() const
+{
+}
+
+void List::validation() const
+{
+}
+
+void List::inputText()
+{
+}
+
+void List::textToInt()
+{
+}
+
+void List::addLector(int id, std::string)
+{
+}
+
+void List::removeLector(int id)
+{
+}
+
+void List::changeState(int id, char state_1, char state_2)
+{
+}
--- LEKTOR/PC/PC/List.h
+++ LEKTOR/PC/PC/List.h
@@ -1,13 +1,41 @@
 #pragma once
+
+//Packages
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+
 class List
 {
 public:
-	List();
-	~List();
+	List();		//Default constructor
+	~List();	//Destructor
+
+	//Mutators
 	void setSize(int size);
-	int getSize();
+	int getSize() const;
+	void setLine(std::string line);
+	std::string getLine() const;
+
+	//Methods
+	void initializeSpaces(int size);
+	void printAll() const;
+	void validation() const;
+	void inputText();
+	void textToInt();
+	
+	//Add lector from name
+	void addLector(int id, std::string);
+	void removeLector(int id);
+	void changeState(int id, char state_1, char state_2);
 
 private:
-	int size_;
+	int size_; //Changeable
+	std::string* storage_; //Dynamic
+	std::string line_;
+	std::fstream myFile_;
+
 };
 
