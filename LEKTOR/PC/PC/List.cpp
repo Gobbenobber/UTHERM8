c9ac931a0d51c6629244f42eadb288be6f36f53f
reklezz <drelixgaming@gmail.com>
2017-05-24 10:05:24 +0200

    added initialize

--- LEKTOR/PC/PC/List.cpp
+++ LEKTOR/PC/PC/List.cpp
@@ -7,7 +7,7 @@ List::List()
 	size_ = 100; //100 lektorKontorer.
 	storage_ = new std::string[size_];
 	initializeSpaces(size_);
-
+	printAll();
 }
 
 
@@ -18,6 +18,14 @@ List::~List()
 
 void List::setSize(int size)
 {
+	if (size < 1000 && size > 0)
+	{
+		size_ = size;
+	}
+	else
+	{
+		std::cout << "Error:1 - input a whole number between 0 and 1000" << std::endl;
+	}
 }
 
 int List::getSize() const
@@ -27,6 +35,7 @@ int List::getSize() const
 
 void List::setLine(std::string line)
 {
+	line_ = line_;
 }
 
 std::string List::getLine() const
@@ -36,6 +45,29 @@ std::string List::getLine() const
 
 void List::initializeSpaces(int size)
 {
+	myFile_.open("Text.txt");
+	//Prepare indicators
+	std::cout << "Inputting indicators\n";
+	if (myFile_.is_open())
+	{
+		for (int i = 0; i < size; i++)
+		{
+			if (i < 10)
+			{
+				myFile_ << "a0" << i << "-\n";
+			}
+			else
+			{
+				myFile_ << "a" << i << "-\n";
+			}
+		}
+		std::cout << "Path has been prepared and path is open" << std::endl;
+	}
+	else
+		std::cout << "path is closed" << std::endl;
+
+	//Close the file
+	myFile_.close();
 }
 
 void List::printAll() const
