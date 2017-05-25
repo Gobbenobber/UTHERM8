c4eb3f55a7ee72fc1f82724c13961ed62db99e66
reklezz <drelixgaming@gmail.com>
2017-05-25 17:59:32 +0200

    more info

--- LEKTOR/PC/PC/Source1.cpp
+++ LEKTOR/PC/PC/Source1.cpp
@@ -10,7 +10,7 @@ int main()
 	//Receiver
 	const std::string COMPort_1 = "COM3";
 
-	std::cout << "Seriel kommunikation ### - 1 - ###\n\n" << std::endl;
+	std::cout << "Seriel kommunikation ### - 1 - ###\n" << std::endl;
 	Serial* arduinoPtr_1 = new Serial("COM3");			// adjust as needed
 
 	std::cout << "Arduino_1 Receiver on the " << COMPort_1 << " port"<< std::endl;
@@ -21,7 +21,7 @@ int main()
 	//Transmitter
 	const std::string COMPort_2 = "COM4";
 
-	std::cout << "Seriel kommunikation ### - 2 - ###\n\n" << std::endl;
+	std::cout << "Seriel kommunikation ### - 2 - ###\n" << std::endl;
 	Serial* arduinoPtr_2 = new Serial("COM4");			// adjust as needed
 
 	std::cout << "Arduino_2 Receiver on the " << COMPort_2 << " port" << std::endl;
@@ -44,7 +44,9 @@ int main()
 	int tempEdit;
 	int menuIndex = 0;
 	bool edit = false;
-	std::string tempString;
+	std::string tempString = "  ";	//insert two spaces
+	std::string holderString;
+	int tempInt;
 
 	std::cout << "### - Admin menu, choose 1 to edit or 0 to continue - ###" << std::endl;
 	std::cout << "= ";
@@ -74,7 +76,58 @@ int main()
 			switch (menuIndex)
 			{
 			case 1:
-				//cout << 
+				std::cout << "### --------- Create a Lector ------ ###" << std::endl;
+				std::cout << "### ----- Enter a lector in the ---- ###" << std::endl;
+				std::cout << "### ------- following syntax: ------ ###" << std::endl;
+
+				//Initials
+				std::cout << std::endl;
+				std::cout << "### ---------- Initials {XX} ------- ###" << std::endl;
+				do
+				{
+					std::cout << "= ";
+					std::cin >> tempString;
+					tempString.resize(2);
+				} while (tempString[0] < 'A' || tempString[0] > 'Z' || tempString[1] < 'A' || tempString[1] > 'Z' );
+				std::cout << "& You've entered: " << tempString << std::endl;
+				holderString = tempString;	//Copy string
+				tempString = "";
+
+				//Name
+				std::cout << std::endl;
+				std::cout << "### ----------- First Name --------- ###" << std::endl;
+				std::cout << "### ----------- {Henning} ---------- ###" << std::endl;
+				do
+				{
+					std::cout << "= ";
+					std::cin >> tempString;
+					for (int i = 0; i < tempString.size(); i++)
+					{
+						if (tempString[i] == ' ')
+						{
+							tempString = "";
+						}
+						if ((tempString[i] < 'A' || tempString[i] > 'Z') && (tempString[i] < 'a' || tempString[i] > 'Z'))
+						{
+							tempString = "";
+						}
+					}
+
+				} while (tempString.size() <= 1);
+				std::cout << "& You've entered: " << tempString << std::endl;
+
+				std::cout << "### ------ You've successfully  ---- ###" << std::endl;
+				std::cout << "### -------- entered a lector ------ ###" << std::endl;
+				std::cout << "### --- Now enter the correct ID --- ###" << std::endl;
+				std::cout << "### --------- Between 0-255 -------- ###" << std::endl;
+				std::cout << "### --- Only enter whole numbers --- ###" << std::endl;
+
+				do
+				{
+					std::cout << "= ";
+					std::cin >> tempInt;
+				} while (tempInt < 0 || tempInt > 255);
+
 				//adminSys.addLector();
 			break;
 			}
